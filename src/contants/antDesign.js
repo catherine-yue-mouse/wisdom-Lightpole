95bbe37cd2b345818e70ffe7544f60fc132a5d2e
yuejinrong <yuejinrong@uindata.com>
2020-11-06 17:41:58 +0800

    初始化提交

---
+++ src/contants/antDesign.js
@@ -0,0 +1,3 @@
+import App from '../App.vue'
+import * as ant  from 'ant-design-vue';
+App.use(ant.Button);
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+const app = createApp(App)
+console.log(app)
+app.use(Antd);
+
+// createApp(App)
+//     .use(Antd) 
+//     .mount('#app')
